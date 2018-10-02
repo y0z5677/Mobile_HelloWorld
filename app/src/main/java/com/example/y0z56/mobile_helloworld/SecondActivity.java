5ab50685a6c832a0644c302eb59f696b6a7e53ef
y0z5677 <christinezhang@att.net>
2018-10-01 21:27:06 -0500

    commit new code

---
+++ app/src/main/java/com/example/y0z56/mobile_helloworld/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.y0z56.mobile_helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        showRandomNumber();
+    }
+
+    public void showRandomNumber () {
+
+    }
+}
